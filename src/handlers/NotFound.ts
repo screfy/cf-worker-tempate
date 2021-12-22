7d28f0b372f8fedc591a90e2fc5905688ca2fcab
screfy <screfy@pm.me>
2021-12-22 17:22:51 +0100

    feat: add NotFound handler

---
+++ src/handlers/NotFound.ts
@@ -0,0 +1,5 @@
+import { HttpRequest, HttpResponse } from '../types'
+
+export default function (req: HttpRequest, res: HttpResponse) {
+  return res.status(404).send({ message: 'route not found' })
+}
